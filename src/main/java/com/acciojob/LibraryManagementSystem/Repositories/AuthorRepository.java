8f2ba774bc966ba888a97b8ca82e172a453e45c5
“AmeyGhatale” <ameyghatale2016@gmail.com>
2024-05-03 10:46:45 +0530

    Commit

--- src/main/java/com/acciojob/LibraryManagementSystem/Repositories/AuthorRepository.java
+++ src/main/java/com/acciojob/LibraryManagementSystem/Repositories/AuthorRepository.java
@@ -7,3 +7,36 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
 }
+
+/*
+*
+* @Mock
+    private AuthorRepository authorRepository;
+
+    @InjectMocks
+    public AuthorService authorService; //automatically creating an object of the service layer
+    // AuthorService authorService = new AuthorService();
+
+
+    @Test
+    public void testMaxBooksAuthor(){
+
+
+
+        Author a1 = new Author(1,3);
+        Author a2 = new Author(2,5);
+        Author a3 = new Author(3,7);
+
+        List<Author> authorList = new ArrayList<>();
+        authorList.add(a1);
+        authorList.add(a2);
+        authorList.add(a3);
+
+        Mockito.when(authorRepository.findAll()).thenReturn(authorList);
+
+        Author actualAuthor = authorService.getAuthorWithMaxBooks();
+
+        Assertions.assertEquals(a3,actualAuthor);
+
+*
+* */
